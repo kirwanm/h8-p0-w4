4d16c2ebf84c1a9ca3bdd0eb64b5d45a21593d0e
kirwanm <kirwanm@yahoo.com>
2019-10-11 14:38:47 +0700

    Submit Week 4 Exercise 15 & 16 v2

---
+++ p0-w4-e15-highestScore-v2.js
@@ -0,0 +1,95 @@
+//Week-4 Exercise 15 Highest Score
+/*
+Implementasikan function highestScore untuk mendapatkan student dengan nilai tertinggi dari setiap class:
+Output yang diharapkan berupa Object Literal dengan format sebagai berikut:
+
+{
+  <class>: { name: <name>, score: <score> },
+  <class>: { name: <name>, score: <score> },
+  <class>: { name: <name>, score: <score> }
+}
+*/
+
+//Object Literal
+function highestScore(students) {
+  var objClass = {};
+
+  for(i=0; i<students.length; i++) {
+    if(objClass[students[i]['class']]===undefined) {
+      objClass[students[i]['class']] = {name: students[i]['name'], score: students[i]['score']}
+    }
+    else if(students[i]['score']>objClass[students[i]['class']]['score']) {
+      objClass[students[i]['class']]['name']=students[i]['name'],
+      objClass[students[i]['class']]['score']=students[i]['score']
+    }
+  }
+  //console.log(objClass);
+  return objClass;
+}
+  
+  console.log(highestScore([
+    {
+      name: 'Dimitri',
+      score: 90,
+      class: 'foxes'
+    },
+    {
+      name: 'Alexei',
+      score: 85,
+      class: 'wolves'
+    },
+    {
+      name: 'Sergei',
+      score: 74,
+      class: 'foxes'
+    },
+    {
+      name: 'Anastasia',
+      score: 78,
+      class: 'wolves'
+    }
+  ]));
+  
+  // {
+  //   foxes: { name: 'Dimitri', score: 90 },
+  //   wolves: { name: 'Alexei', score: 85 }
+  // }
+  
+  
+  console.log(highestScore([
+    {
+      name: 'Alexander',
+      score: 100,
+      class: 'foxes'
+    },
+    {
+      name: 'Alisa',
+      score: 76,
+      class: 'wolves'
+    },
+    {
+      name: 'Vladimir',
+      score: 92,
+      class: 'foxes'
+    },
+    {
+      name: 'Albert',
+      score: 71,
+      class: 'wolves'
+    },
+    {
+      name: 'Viktor',
+      score: 80,
+      class: 'tigers'
+    }
+  ]));
+  
+  // {
+  //   foxes: { name: 'Alexander', score: 100 },
+  //   wolves: { name: 'Alisa', score: 76 },
+  //   tigers: { name: 'Viktor', score: 80 }
+  // }
+  
+  
+  console.log(highestScore([])); //{}
+  
